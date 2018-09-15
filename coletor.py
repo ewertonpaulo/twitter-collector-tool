369fbe69ab451fc391332b6eac745cad275a399b
ewertonpaulo <ewerton.paulo@dce.ufpb.br>
2018-09-15 20:33:59 -0300

    Update coletor.py
    Sintaxe fixes

--- coletor.py
+++ coletor.py
@@ -1,41 +1,37 @@
 import tweepy
-import json
 
 #Autenticações
 consumer_key = 'JItXCDXKcCqulu8vx58Go6eZg'
 consumer_secret = 'Vlrza3KfonxqMpvzr4TDSG0cmrN9o6WBFkJrFAPxKuQVf5hjY3'
 access_token = '750123144-tqP5pv6sHS4YTlBQKjy6CYZA9tD6JYuITluOzxeE'
 access_token_secret = 'Eb8iNY45Z8JTTt7iytewOw5gdSTamWbddTZmeEUEWB8ue'
-
 # Listener of tweets
-class PrintListener(tweepy.StreamListener):
+class Listener(tweepy.StreamListener):
 
     def on_status(self, status):
         if hasattr(status, 'retweeted_status'):
             try:
-                tweet = status.retweeted_status.extended_tweet["full_text"]
+                text = status.retweeted_status.extended_tweet["full_text"]
             except:
-                tweet = status.retweeted_status.text
+                text = status.retweeted_status.text
         else:
             try:
-                tweet = status.extended_tweet["full_text"]
+                text = status.extended_tweet["full_text"]
             except AttributeError:
-                tweet = status.text
+                text = status.text
                 
         user = status.user
         image = status.user.profile_image_url
         followers = status.user.followers_count
         location = status.user.location
-        
-        print(str(user.screen_name) + " => " + str(tweet.encode("ascii","ignore")))
+    
+        print(str(user.screen_name) + " => " + str(text.encode("ascii","ignore")))
     def on_error(self, status):
         print(status)
 
 if __name__ == '__main__':
-    listener = PrintListener()
-
+    listener = Listener()
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    
     stream = tweepy.Stream(auth, listener)
-    stream.filter(follow=None, track=['lula'], languages=["pt"])
+    stream.filter(track=['lula'], languages=["pt"])
