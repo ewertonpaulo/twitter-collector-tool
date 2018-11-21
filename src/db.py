b5737994dfeda203acc4b6c3a21cbfd7c7a2db28
ewertonP <ewerton.paulo@dce.ufpb.br>
2018-11-21 15:44:46 -0300

    find close matches

--- src/db.py
+++ src/db.py
@@ -1,4 +1,4 @@
-import psycopg2
+import psycopg2, difflib
 from auth import dbname,host,password,port,user
 
 class Database:
@@ -31,10 +31,13 @@ class Database:
             pass
 
     def find(self, text):
-        sql = "SELECT text FROM public.tweet WHERE text = '%s' ORDER BY id ASC " %text
+        sql = "SELECT text FROM public.tweet ORDER BY id ASC"
         self.cursor.execute(sql)
-        rs = self.cursor.fetchall();
-        if len(rs)==0:
-            return True
-        else:
+        all = [r[0] for r in self.cursor.fetchall()]
+        print(all)
+        diff = difflib.get_close_matches(text, all)
+        if diff:
+            print(diff)
             return False
+        else:
+            return True
